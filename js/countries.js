2999289d6caae1351ed3526de949a6f96749ed69
Imran Hossen <freelancerimran81@gmail.com>
2022-02-23 19:47:26 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,44 @@
+
+const loadCountries = () => {
+    // fetch('https://restcountries.eu/rest/v2/all')
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = counties => {
+    // for (const country of counties) {
+    //     console.log(country);
+    // }
+    const countriesDiv = document.getElementById('countries');
+    counties.forEach(country => {
+        // console.log(country);
+        const div = document.createElement('div');
+        div.classList.add('country')
+        div.innerHTML = `
+            <h3>${country.name.common}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+        `;
+        countriesDiv.appendChild(div);
+    });
+}
+
+const loadCountryByName = name => {
+    // const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]));
+}
+
+const displayCountryDetail = country => {
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h4>${country.name.common}</h4>
+        <p>population: ${country.population}</p>
+        <img width="200px" src="${country.flags.png}">
+    `
+}
+
